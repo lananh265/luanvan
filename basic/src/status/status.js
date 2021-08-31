c64eea5f20a5cb781c5005aa0bec9909d36a06d9
lananh265 <htlanhd20105@cusc.ctu.edu.vn>
2021-08-31 23:44:51 +0700

    post status duoc

--- basic/src/status/status.js
+++ basic/src/status/status.js
@@ -4,13 +4,16 @@ import {useState, useEffect} from 'react';
 export default function Status(){
     const [status, setStatus] = useState([""]);
     const [newStatus, setNewStatus] = useState("");
+    const [trangthai, setTrangThai] = useState("");
+
+
 
     const listStatus = status.map( (element,index) =>{
         return <h3 key={index}>{element.userId }{' '}{element.desc} </h3>
     })  
 
     const postStatus = ()=>{
-        const url = "http://localhost:8080/poststatus";
+        const url = "http://localhost:8081/status";
         const json = {
             userId: 1,
             desc: newStatus
@@ -20,18 +23,21 @@ export default function Status(){
             headers: {
                 'Content-Type': 'application/json'
               },
-              body: JSON.stringify()
+              body: JSON.stringify(json)
         })
-        .then( data => data.json())
+        .then( (data)=>data.json())
         .then( (dataJson)=>{
-            console.log(dataJson);
+            setTrangThai(dataJson.desc);
+            setStatus([dataJson,...status]); 
         })
+        
         .catch( (loi)=>{
             console.log(loi)
         })
     }
 
     useEffect ( ()=>{
+        console.log("run UseEffect")
         const url = "http://localhost:8080/login";
         fetch(url, {
             method: "GET"
@@ -49,13 +55,16 @@ export default function Status(){
     return(
         <div>
             <h1>Hãy chia sẻ trạng thái mới của bạn</h1>
-            <form onSubmit={ ()=>{postStatus()}}>
+            <form >
                 <label>
                     <input type="text" onChange={ (e)=> setNewStatus(e.target.value)}></input>
                 </label>
-                <button type="submit"> Đăng bài</button>
+                
             </form>
+
+            <button onClick={()=>{postStatus()}}> Đăng bài</button>
             {listStatus}
+            
         </div>
     )
 }
